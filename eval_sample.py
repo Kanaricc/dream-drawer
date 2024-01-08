e6eabbc84198278daa8360f03ad4423855943918
Kanaricc <iovo7c@gmail.com>
2024-01-09 00:07:04 +0800

    chore: 1

--- eval_sample.py
+++ eval_sample.py
@@ -7,9 +7,9 @@ from torchvision.utils import save_image
 from chinopie import logger
 from chinopie.filehelper import InstanceFileHelper,GlobalFileHelper
 
-from sd_hook import patch_pipe
+from sd_hook import patch_pipe,tune_lora_scale
 
-pipe = StableDiffusionPipeline.from_single_file('base_models/meinamix_meinaV11.safetensors')#.to('cuda')
+pipe = StableDiffusionPipeline.from_single_file('base_models/meinamix_meinaV11.safetensors',local_files_only=True).to('cuda')
 pipe.safety_checker=None
 logger.warning("loaded pipeline")
 
@@ -18,14 +18,13 @@ logger.warning("loaded scheduler")
 
 helper=GlobalFileHelper('deps')
 
-for i in range(0,300,10):
-    ti_ckpt=torch.load(helper.get_exp_instance('alpha(0)_trial1').find_latest_checkpoint())['model']
-    # lora_unet_ckpt=torch.load(helper.get_exp_instance('alpha(1)').get_checkpoint_slot(i))['model']
-    patch_pipe(pipe,ti_ckpt=ti_ckpt)
+ti_ckpt=torch.load(helper.get_exp_instance('alpha(0)_trial1').find_latest_checkpoint())['model']
+lora_unet_ckpt=torch.load(helper.get_exp_instance('alpha(1)_trial1').find_latest_checkpoint())['model']
+patch_pipe(pipe,ti_ckpt=ti_ckpt,unet_ckpt=lora_unet_ckpt)
 
-    prompt = "a girl in the style of <clear>"
-    torch.manual_seed(0) # !
-    image:Image = pipe(prompt, num_inference_steps=40, guidance_scale=7,num_images_per_prompt=1).images[0]
+prompt = "a girl, white hair, sit on chair, short skirt, cat ear"
+for i in range(10):
+    torch.manual_seed(i) # !
+    image:Image = pipe(prompt, num_inference_steps=50, guidance_scale=7,num_images_per_prompt=1,clip_skip=2).images[0]
 
-    image.save(f'test/test-{i}.jpg')
-    break
+    image.save(f'test-{i}.jpg')
